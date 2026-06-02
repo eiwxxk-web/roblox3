1b8755267073466aded33558874cc1e07a30d141
eiwxxk-web <eiwxxk@gmail.com>
2026-06-03 01:51:10 +0400

    Update server.js

--- server.js
+++ server.js
@@ -10,5 +10,5 @@ app.get('*', (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`NexMeet running on port ${PORT}`);
+  console.log('NexMeet running on port ' + PORT);
 });
